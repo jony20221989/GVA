3f0c65544485bcb361bcb3e6a844ac91104a117e
jony20221989 <jony20221989@gmail.com>
2022-10-27 01:48:09 +0800

    前端框架搭建

--- web-admin/src/App.vue
+++ web-admin/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {getCaptcha} from '@/api/base'
 
 export default {
   name: 'App',
+  created() {
+    getCaptcha();
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
